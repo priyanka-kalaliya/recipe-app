65a9a723cddaeba0b0195bfd71631c7ef8a38f5e
Priyanka Kalaliya <priyayadhuvanshi55@gmail.com>
2023-07-11 14:36:00 +0530

    Add files via upload

---
+++ assets/js/module.js
@@ -0,0 +1,7 @@
+/**
+ * @license MIT
+ * @copyright 2023 Priyanka
+ * @author Priyanka <priyayadhuvanshi55@gmail.com>
+ */
+
+"use strict";
---
+++ assets/js/theme.js
@@ -0,0 +1,7 @@
+/**
+ * @license MIT
+ * @author Priyanka <priyayadhuvanshi55@gmail.com>
+ * @copyright Priyanka 2023
+ */
+
+"use strict";
